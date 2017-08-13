d9e9ae95e489543fdaba034426db380efa378edc
james <james@gmail.com>
2017-08-13 16:00:25 +0800

    完善

--- phippy_python.py
+++ phippy_python.py
@@ -504,7 +504,15 @@ class User(Base):
     user_id = Column(String(40), unique=True)
     email = Column(String(120), unique=False)
 
-    first_time = Column(DateTime, unique=False)
+    # 国籍
+    nationality = Column(String(20), unique=False)
+
+    # 注册时间
+    register_time = Column(DateTime, unique=False)
+
+    # 最后登录时间
+    last_time = Column(DateTime, unique=False)
+
 
     status = Column(Integer, unique=False)
     uuid = Column(String(120), unique=False)
