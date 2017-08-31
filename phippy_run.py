774a8686431aa97f1ac4d753ed21452add00d29d
james <james@gmail.com>
2017-08-31 17:58:07 +0800

    打开 appstore

--- phippy_run.py
+++ phippy_run.py
@@ -7,4 +7,4 @@ from phippy import app
 if __name__ == '__main__':
     app.run('10.71.12.139', port=5001)
     # app.run('127.0.0.1', port=5001)
-    # https: // itunes.apple.com / ph / app / wechat / id414478124?mt = 8
+    # https: // itunes.apple.com / ph / app / wechat / id41447da8124?mt = 8
